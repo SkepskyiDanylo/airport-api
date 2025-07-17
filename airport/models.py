7ad911b585aecb6a8f38de9fa522e8a7eb3ec77a
Danylo Skepskyi <kol230305@gmail.com>
2025-07-17 22:00:38 +0200

    Dev: improved airport models
    - Added role to Crew
    - Added manufacturer/status/tail_number/model/last_inspection and deleted name for Airplane
    - Added IATA/ICAO codes + timezone/latitude/longitude for Airport

--- airport/models.py
+++ airport/models.py
@@ -1,7 +1,10 @@
 import uuid
 
+import pytz
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import TextChoices
+from django.utils.translation import gettext as _
 
 
 class BaseModel(models.Model):
@@ -20,8 +23,31 @@ class AirplaneType(BaseModel):
 
 
 class Airplane(BaseModel):
-    name = models.CharField(max_length=255)
+    MANUFACTURER_CHOICES = (
+        ("AIRBUS", _("Airbus")),
+        ("BOEING", _("Boeing")),
+        ("EMBRAER", _("Embraer")),
+        ("BOMBARDIER", _("Bombardier")),
+        ("ATR", _("ATR")),
+        ("SUKHOI", _("Sukhoi")),
+        ("COMAC", _("COMAC")),
+        ("MCDONNELL_DOUGLAS", _("McDonnell Douglas")),
+        ("LOCKHEED", _("Lockheed")),
+        ("IL", _("Ilyushin")),
+        ("TU", _("Tupolev")),
+        ("ANTONOV", _("Antonov")),
+    )
+    STATUS_CHOICES = (
+        ("ACTIVE", _("Active")),
+        ("INACTIVE", _("Inactive")),
+        ("FROZEN", _("Frozen")),
+    )
     type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE, related_name="airplanes")
+    tail_number = models.CharField(max_length=255, unique=True)
+    manufacturer = models.CharField(max_length=30, choices=MANUFACTURER_CHOICES)
+    model = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="ACTIVE")
+    last_inspection = models.DateTimeField(null=True, blank=True)
     rows = models.IntegerField(
         validators=[
             MinValueValidator(1),
@@ -34,7 +60,7 @@ class Airplane(BaseModel):
     )
 
     def __str__(self):
-        return self.name
+        return f"{self.manufacturer} {self.model}: {self.tail_number}"
 
     @property
     def total_seats(self):
@@ -42,16 +68,40 @@ class Airplane(BaseModel):
 
 
 class Crew(BaseModel):
+    ROLE_CHOICES = (
+        ("PILOT", _("Pilot"),),
+        ("CO-PILOT", _("Co-pilot"),),
+        ("FLIGHT_ATTENDANT", _("Flight Attendant"),),
+        ("ENGINEER", _("Engineer"),),
+    )
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.role}: {self.first_name} {self.last_name}"
 
 
 class Airport(BaseModel):
     name = models.CharField(max_length=255)
+    IATA_code = models.CharField(max_length=3, unique=True)
+    ICAO_code = models.CharField(max_length=4, unique=True)
     closest_big_city = models.CharField(max_length=255)
+    timezone = models.CharField(
+        max_length=50,
+        choices=[(tz, tz) for tz in pytz.common_timezones],
+        default="UTC"
+    )
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6,
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6,
+    )
+
+    def __str__(self):
+        return f"{self.IATA_code} - {self.ICAO_code} - {self.name}"
 
 
 class Route(BaseModel):
